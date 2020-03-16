ece60232d7aa30ebe176877db5cb7c21b62a0d50
Jissmon Jose <37608527+jisshub@users.noreply.github.com>
2020-03-16 13:28:40 +0530

    updated super keyword file

--- super_keyword.py
+++ super_keyword.py
@@ -7,5 +7,6 @@ class Dog(Mammal):
     print('Dog has four legs.')
     # Here, we called the __init__() method of the Mammal class (from the Dog class) using code
     super().__init__('Dog')
-    
+  
+#create a instance of dog class - no need of passing argument mammalName 
 d1 = Dog()
